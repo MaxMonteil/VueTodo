d722c844ca62fa1fc8fb440a57701533103c0283
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-01-14 00:48:28 +0200

    Place TodoForm component inside ListItem.

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,25 +1,38 @@
 <template>
 <li class="todo-item" :class="{ completed: isCompleted }">
-  <p>{{ todo.priority }} | {{ todo.title }}</p>
-  <span>
-    <button type="button" class ="delete-button" @click="deleteTodo">X</button>
-    <button type="button" class ="edit-button" @click="editTodo" v-if="!isCompleted">
-      <svg aria-hidden="true" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="-380 -380 1280 1280"><path fill="currentColor" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"></path></svg>
-    </button>
-    <button type="button" class ="complete-button" @click="completeTodo" v-if="!isCompleted">✓</button>
-  </span>
+  <div class="todo-content" v-if="populateWith.empty">
+    <p>{{ todo.priority }} | {{ todo.title }}</p>
+    <span>
+      <button type="button" class ="delete-button" @click="deleteTodo">X</button>
+      <button type="button" class ="edit-button" @click="editTodo" v-if="!isCompleted">
+        <svg aria-hidden="true" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="-380 -380 1280 1280"><path fill="currentColor" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"></path></svg>
+      </button>
+      <button type="button" class ="complete-button" @click="completeTodo" v-if="!isCompleted">✓</button>
+    </span>
+  </div>
+
+  <todo-form v-else/>
 </li>
 </template>
 
 <script>
+import TodoForm from './TodoForm.vue'
+
 export default {
   name: 'ListItem',
   props: {
+    populateWith: {
+      type: Object,
+      default: () => ({ empty: true })
+    },
     todo: Object,
     isCompleted: {
       type: Boolean,
       default: false
     }
+  },
+  components: {
+    TodoForm
   },
   methods: {
     deleteTodo() {
@@ -37,14 +50,17 @@ export default {
 
 <style scoped>
 .todo-item {
-  display: flex;
-  justify-content: space-between;
   margin: 16px 0;
   padding: 8px;
   padding-left: 16px;
   background-color: #42b983;
   border-radius: 8px;
   color: white;
+}
+
+.todo-content {
+  display: flex;
+  justify-content: space-between;
 }
 
 span {
@@ -88,7 +104,7 @@ button:active {
   background-color: rgba(66, 185, 131, 0.4);
 }
 
-.completed > p{
+.completed > p {
   text-decoration: line-through;
 }
 </style>
