879928e225b79175e9b7c1c81496d98aed607ed8
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-02-09 22:34:30 +0200

    Streamline export statements.

--- src/api/localStorageService.js
+++ src/api/localStorageService.js
@@ -1,17 +1,12 @@
-const checkStorage = key => {
-  if (localStorage.getItem(key)) {
+export const checkStorage = key => {
+  if (window.localStorage.getItem(key)) {
     try {
-      return JSON.parse(localStorage.getItem(key))
+      return JSON.parse(window.localStorage.getItem(key))
     } catch (e) {
-      localStorage.removeItem(key)
+      window.localStorage.removeItem(key)
       throw e
     }
   }
 }
 
-const saveToStorage = (key, value) => localStorage.setItem(key, JSON.stringify(value))
-
-export {
-  checkStorage,
-  saveToStorage
-}
+export const saveToStorage = (key, value) => window.localStorage.setItem(key, JSON.stringify(value))
